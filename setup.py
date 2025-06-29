87a539489dfb0d32ea9353c37485ec52bfe348ea
KNagaveni <131789386+KNagaveni@users.noreply.github.com>
2025-06-29 21:51:23 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'KNagaveni',
+    author_email= 'katamveni88@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
